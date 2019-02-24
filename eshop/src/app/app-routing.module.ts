23ed88dac60b21e556cff1457fe31cdc21bc3e77
SatheeshKumar S <satheeshkumar.s@platform3solutions.com>
2019-02-24 23:20:43 +0530

    firebase intergrated

--- eshop/src/app/app-routing.module.ts
+++ eshop/src/app/app-routing.module.ts
@@ -1,10 +1,30 @@
-import { NgModule } from '@angular/core';
+import { NgModule, Component } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { CartComponent } from './cart/cart.component';
+import { LoginComponent } from './login/login.component';
+import { OrderSuccessComponent } from './order-success/order-success.component';
+import { CheckoutComponent } from './checkout/checkout.component';
+import { ProductsComponent } from './products/products.component';
+import { MyOrderComponent } from './my-order/my-order.component';
+import { AdminOrderComponent } from './admin/admin-order/admin-order.component';
+import { AdminProductComponent } from './admin/admin-product/admin-product.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '' , component: HomeComponent },
+  { path: 'cart', component: CartComponent},
+  { path: 'login', component: LoginComponent },
+  { path: 'order-success', component: OrderSuccessComponent },
+  { path: 'checkout', component: CheckoutComponent },
+  { path: 'products', component: ProductsComponent },
+  { path: 'my-order', component: MyOrderComponent },
+  { path: 'admin-orders', component: AdminOrderComponent },
+  { path: 'admin-products', component: AdminProductComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+ }
--- eshop/src/app/app.module.ts
+++ eshop/src/app/app.module.ts
@@ -8,11 +8,29 @@ import {AngularFireAuthModule} from '@angular/fire/auth';
 import {AngularFireDatabaseModule} from '@angular/fire/database';
 import { environment } from 'src/environments/environment';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { HomeComponent } from './home/home.component';
+import { CartComponent } from './cart/cart.component';
+import { LoginComponent } from './login/login.component';
+import { ProductsComponent } from './products/products.component';
+import { CheckoutComponent } from './checkout/checkout.component';
+import { OrderSuccessComponent } from './order-success/order-success.component';
+import { MyOrderComponent } from './my-order/my-order.component';
+import { AdminOrderComponent } from './admin/admin-order/admin-order.component';
+import { AdminProductComponent } from './admin/admin-product/admin-product.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavBarComponent
+    NavBarComponent,
+    HomeComponent,
+    CartComponent,
+    LoginComponent,
+    ProductsComponent,
+    CheckoutComponent,
+    OrderSuccessComponent,
+    MyOrderComponent,
+    AdminOrderComponent,
+    AdminProductComponent
   ],
   imports: [
     BrowserModule,

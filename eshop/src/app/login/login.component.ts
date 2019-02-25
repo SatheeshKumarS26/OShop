0a532bdfb56cc5b003bc107bc0ff3764add0164f
SatheeshKumar S <satheeshkumar.s@platform3solutions.com>
2019-02-25 15:01:27 +0530

    adding gauth

--- eshop/src/app/login/login.component.ts
+++ eshop/src/app/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {AngularFireAuth} from '@angular/fire/auth';
+import * as firebase from 'firebase';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +9,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  constructor(private auth: AngularFireAuth) { }
 
   ngOnInit() {
+  }
+
+  login(){
+    this.auth.auth.signInWithRedirect(new firebase.auth.GoogleAuthProvider());
   }
 
 }
